ecb5ba9719660432310edfcdb5615f27bec931e5
DAVID SWATZELL <davidswatzell@DAVIDs-MacBook-Pro.local>
2020-08-14 20:49:26 -0500

    added everything that i can sort of understand to this point

---
+++ app.js
@@ -0,0 +1,23 @@
+let name = "David";
+const stateAmt = 50;
+//add function usage
+let numSum = add(5, 4); 
+var veggies = ["corn","spinach","squash","okra"];
+console.log (sayHello());
+console.log (numSum);
+function add (num1, num2){
+    return num1 + num2
+};
+function sayHello () {
+    alert ("Hello World!!");
+}
+for (let i of veggies) {
+    console.log(i);
+}
+
+let pet = {
+    name: "Major",
+    breed: "Pug"
+};
+console.log (pet.name)
+console.log (pet.breed)
